f20c178656a0e6d10ea2b1578dcbcc899852ba4d
Alessandro Vioni <jenoma@gmail.com>
2015-02-01 16:00:15 +0100

    Added auto compile script for MacVim release

---
+++ auto-compile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Basic
+cd ~/source-macvim
+echo "n
+4
+y" | ./compile-macvim.sh
+cd ~/macvim/src/MacVim/build/Release/
+tar -cvzf MacVim-Basic.tar.gz MacVim.app
+
+
+# Complete with Yosemite Python, Ruby
+brew unlink ruby
+cd ~/source-macvim
+echo "n
+2
+y" | ./compile-macvim.sh
+cd ~/macvim/src/MacVim/build/Release/
+tar -cvzf MacVim-Yosemite-Python-Ruby-Perl.tar.gz MacVim.app
+
+# Complete with Homebrew Python, Ruby, Lua
+brew link ruby
+cd ~/source-macvim
+echo "y
+1
+y" | ./compile-macvim.sh
+cd ~/macvim/src/MacVim/build/Release/
+tar -cvzf MacVim-Homebrew-Python-Ruby-Lua-Perl-Latest.tar.gz MacVim.app
